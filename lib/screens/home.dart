ee27fe50d8ef55fad7a30c603846fff359363eef
devfaizan <mfaizan.zahir@gmail.com>
2024-09-29 01:28:25 +0500

    added & removed some comments

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,31 +6,28 @@ import 'package:path_provider/path_provider.dart';
 class CounterStorage {
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
-    print(directory.path);
+    print(directory.path); // for debugging
     return directory.path;
   }
 
   Future<File> get _localFile async {
     final path = await _localPath;
-    print(path);
+    print(path); // for debugging
     return File('$path/counter.txt');
   }
 
   Future<int> readCounter() async {
     try {
       final file = await _localFile;
-      // Read the file
       final contents = await file.readAsString();
       return int.parse(contents);
     } catch (e) {
-      // If encountering an error, return 0
       return 0;
     }
   }
 
   Future<File> writeCounter(int counter) async {
     final file = await _localFile;
-    // Write the file
     return file.writeAsString('$counter');
   }
 }
@@ -60,7 +57,6 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _counter++;
     });
-    // Write the variable as a string to the file.
     return _storage.writeCounter(_counter);
   }
 
